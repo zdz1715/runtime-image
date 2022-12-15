b9ab90050ed8d8df3ea7d51e443dbfbd7e089006
zhudz <zdzserver@163.com>
2022-12-15 13:44:49 +0800

    修复环境变量重置的问题

--- tools/cron-log.sh
+++ tools/cron-log.sh
@@ -6,16 +6,15 @@ CRON_PID=$(cat /var/run/crond.pid)
 IS_LOGIN='--preserve-environment'
 
 step_exec() {
-    shift
     {
-      RESULT=$(su "$IS_LOGIN $EXEC_USER" -s /bin/bash -c "$*" 2>&1);
+      RESULT=$(su $IS_LOGIN $EXEC_USER -s /bin/bash -c "$*" 2>&1);
       RESULT_CODE="$?";
       if [[ "$RESULT_CODE" == 0 ]]; then
         RESULT_TXT="SUCCESS"
       else
         RESULT_TXT="FAILURE"
       fi
-      echo -e "--- [$(date '+%Y-%m-%d %H:%M:%S')] [$USER] [$RESULT_TXT] $*";
+      echo -e "--- [$(date '+%Y-%m-%d %H:%M:%S')] [$EXEC_USER] [$RESULT_TXT] $*";
       echo -e "$RESULT";
     } >> /proc/"${CRON_PID}"/fd/1 2>&1
     return "${PIPESTATUS[0]}"
